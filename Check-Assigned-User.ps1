2a5390d7cfba45fe45cac9ffcce9a213bef593c0
ahmad afif bin che mohamad nor <ahmadafif5321@gmail.com>
2023-08-03 23:15:44 +0800

    Add files via upload
    Checking for VDI list which of the user is assigned and to send data through outlook

---
+++ Check-Assigned-User.ps1
@@ -0,0 +1,17 @@
+﻿Add-PSSnapin Citrix*
+Set-StrictMode -version latest
+
+#Replace the "filepath" with the location of file
+$Path = "filepath"
+$VDIList = Get-Content -Path $Path
+
+$export = [System.Collections.ArrayList]@()
+
+foreach ($VDI in $VDIList) {
+    $brokerMachine = Get-BrokerMachine -MachineName $VDI
+    $output = "User assigned for $VDI : $($brokerMachine.AssociatedUserFullNames)"
+    Write-Host $output
+    $export.Add($output) | Out-Null
+}
+
+$export | Out-GridView -Title "VDI USer assign" -OutputMode Multiple
