419564abc44180f5a9dfae45716c0644b9d88c78
ahmad afif bin che mohamad nor <ahmadafif5321@gmail.com>
2023-08-04 22:41:44 +0800

    Add files via upload
    checking which vdi assigned to user based on the username. It will publish data in ogv

---
+++ Check-Assigned-VDI-based-username.ps1
@@ -0,0 +1,14 @@
+﻿Add-PSSnapin Citrix*
+Set-StrictMode -Version latest
+
+#Replace the "filepath" with the location of file
+$VDIuser = Get-Content -Path "path"
+
+$output = foreach($user in $VDIuser)
+{
+    $username = Get-BrokerDesktop | Where-Object { $_.AssociatedUserFullNames -eq $user }
+    $output = "VDI Assigned for $user : $($username.MachineName)"
+    Write-Output $output
+}
+
+$output | Ogv
